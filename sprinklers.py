b5717833582a27772e0cabc4c79db5e5e5d34b67
brianjohnpolasek <brianjohnpolasek@gmail.com>
2022-10-07 21:19:34 -0500

    Convert constants to uppercase

--- sprinklers.py
+++ sprinklers.py
@@ -3,30 +3,31 @@ import time
 import RPi.GPIO as GPIO
 
 # CONSTANTS
-master_valve = 18
-#valves = [23, 24, 25, 12, 5, 6, 13]
-valves = [23, 24, 25, 5, 6, 13]
+MASTER_VALVE = 18
+#VALVES = [23, 24, 25, 12, 5, 6, 13]
+VALVES = [23, 24, 25, 5, 6, 13]
+DEFAULT_RUNTIME = 60
 
 # PINOUT SETUP
 def setup_pins():
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
-    GPIO.setup(master_valve, GPIO.OUT, initial=GPIO.HIGH)
-    GPIO.setup(valves, GPIO.OUT, initial=GPIO.HIGH)
+    GPIO.setup(MASTER_VALVE, GPIO.OUT, initial=GPIO.HIGH)
+    GPIO.setup(VALVES, GPIO.OUT, initial=GPIO.HIGH)
 
 def cleanup_pins():
     GPIO.cleanup()
 
-def run_system(run_time=30):
+def run_system(run_time=DEFAULT_RUNTIME):
     """Runs the sprinklers for the provided time"""
     print("Running sprinkler system")
     setup_pins()
     
-    GPIO.output(master_valve, GPIO.LOW)
-    for valve in valves:
+    GPIO.output(MASTER_VALVE, GPIO.LOW)
+    for valve in VALVES:
         #toggle_valve(valve, run_time)
         print("Valve " + str(valve))
-    GPIO.output(master_valve, GPIO.LOW)
+    GPIO.output(MASTER_VALVE, GPIO.LOW)
     
     cleanup_pins()
     print("Sprinkler system finished!")
